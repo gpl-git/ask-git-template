eae0997ee72ee6c1b277aab2f977145c16f2281c
gpl-git <59488112+gpl-git@users.noreply.github.com>
2024-01-07 14:20:09 -0800

    Update TestContext.java

--- src/test/java/support/TestContext.java
+++ src/test/java/support/TestContext.java
@@ -71,7 +71,7 @@ public class TestContext {
                         chromeOptions.addArguments("--disable-gpu");
                     }
                     if (envLinux) {
-                        chromeOptions.setBinary("/usr/bin/google-chrome-stable");
+                        chromeOptions.setBinary("/usr/bin/chromium-browser");
                     }
                     ChromeDriverService service = new ChromeDriverService.Builder()
                             .withLogOutput(System.out)
